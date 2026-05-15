cc173e7f91c139b21f6d157cbfb0d01c3337e827
albariqitarigan <albariqidtarigan@gmail.com>
2026-05-15 22:31:31 +0700

    feat(auth): implement google oauth and welcome email following docs

--- BelajarKUY/routes/web.php
+++ BelajarKUY/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\GoogleController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,4 +18,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+Route::get('/auth/google', [GoogleController::class, 'redirect'])->name('auth.google');
+Route::get('/auth/google-callback', [GoogleController::class, 'callback'])->name('auth.google.callback');
 require __DIR__.'/auth.php';
